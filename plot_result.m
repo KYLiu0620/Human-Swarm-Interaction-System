1511452068aa269093e483fbddc156795af799ae
KY Liu <k242424123@gamil.com>
2019-01-23 22:05:53 +0800

    [update] Change gain of task space scaling for formation.

---
+++ plot_result.m
@@ -0,0 +1,57 @@
+SlaveRobotNum = 6;
+SlaveRobotDOF = 3;
+t= 0:tout(end)/(length(q_s)-1):tout(end);
+%% swarm plot
+
+figure(1)
+subplot(2,2,1) %xy
+hold on
+for num = [1 4 7 10 13 16]
+    plot(q_s(1,num),q_s(1,num+1),'o');
+    plot(q_s(:,num),q_s(:,num+1));
+end
+xlabel('x');
+ylabel('y');
+hold off
+
+subplot(2,2,2) %yz
+hold on
+for num = [1 4 7 10 13 16]
+    plot(q_s(1,num+1),q_s(1,num+2),'o');
+    plot(q_s(:,num+1),q_s(:,num+2));
+end
+xlabel('y');
+ylabel('z');
+hold off
+
+% figure(2)
+subplot(2,2,3) %xz
+hold on
+for num = [1 4 7 10 13 16]
+    plot(q_s(1,num),q_s(1,num+2),'o');
+    plot(q_s(:,num),q_s(:,num+2));
+end
+xlabel('x');
+ylabel('z');
+hold off
+
+% figure(3)
+subplot(2,2,4) %3D
+hold on
+for num = [1 4 7 10 13 16]
+    plot3(q_s(1,num),q_s(1,num+1),q_s(1,num+2),'o');
+    plot3(q_s(:,num),q_s(:,num+1),q_s(:,num+2));
+end
+plot3(X_m(:,1),X_m(:,2),X_m(:,3),'--');
+xlabel('x');
+ylabel('y');
+zlabel('z');
+hold off
+
+figure(2)
+robot=4;
+subplot(2,1,1)
+plot(t,q_s(:,robot));
+subplot(2,1,2)
+plot(t,q_s(:,robot+1));
+%% master plot
